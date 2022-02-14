83364195fea78799b5ebc25864690e47f2d18203
Thanh Long <thaihothanhlong95@gmail.com>
2022-02-14 10:32:50 +0700

    fix error

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const PORT = process.env.PORT || 5000
 const app = express()
 dotenv.config()
 app.use(cors())
+//routes
 const usersRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
 const filmRoute = require('./routes/film')
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+// env
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.DB_CONNECT,() => {
   console.log('connect to mongoose')
@@ -20,10 +20,9 @@ app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+app.use(express.json({ extended: false }))
 app.use('/api/users', usersRoute)
 app.use('/api/auth', authRoute)
 app.use('/api/film', filmRoute)
 
-app.listen(PORT, () => {
-  console.log('server is running')
-})
+app.listen(PORT)
